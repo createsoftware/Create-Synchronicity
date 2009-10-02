3cf3db5cb79b7fb43c17676593b34da0ab3037a8
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2009-10-02 17:32:49 +0000

    Change the way version numbers are set.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@101 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Create Synchronicity/My Project/AssemblyInfo.vb
+++ Create Synchronicity/My Project/AssemblyInfo.vb
@@ -42,7 +42,6 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("2.0.*")> 
-<Assembly: AssemblyFileVersion("2.0.0.0")> 
-<Assembly: AssemblyInformationalVersion("2009.09.30")> 
+<Assembly: AssemblyFileVersion("2.09.10.02")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("")> 
