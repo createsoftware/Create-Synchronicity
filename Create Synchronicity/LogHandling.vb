d38a852c13661190cdbec5aa4bb280c480a564ea
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2009-10-19 18:14:21 +0000

    Add sanity checks and debug options
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@124 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Create Synchronicity/LogHandling.vb
+++ Create Synchronicity/LogHandling.vb
@@ -44,9 +44,28 @@ Class LogHandler
 
     Sub OpenHTMLHeaders(ByRef LogW As IO.StreamWriter)
         LogW.WriteLine("<!DOCTYPE html PUBLIC ""-//W3C//DTD XHTML 1.1//EN"" ""http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"">")
-        LogW.WriteLine("<html xmlns=""http://www.w3.org/1999/xhtml"" xml:lang=""en"">")
+        LogW.WriteLine("<html xmlns=""http://www.w3.org/1999/xhtml"" xml:lang=""en"" encoding=""utf-8"">")
         LogW.WriteLine("	<head>")
         LogW.WriteLine("		<title>Create Synchronicity - Log for " & LogName & "</title>")
+        LogW.WriteLine("		<meta http-equiv=""Content-Type"" content=""text/html;charset=utf-8"" />")
+        LogW.WriteLine("		<style type=""text/css"">")
+        LogW.WriteLine("			body {")
+        LogW.WriteLine("				font-family: verdana, courier;")
+        LogW.WriteLine("				font-size: 0.8em;")
+        LogW.WriteLine("				margin: auto;")
+        LogW.WriteLine("				width: 60%;")
+        LogW.WriteLine("			}")
+        LogW.WriteLine("			")
+        LogW.WriteLine("			table {")
+        LogW.WriteLine("				border-collapse: collapse;")
+        LogW.WriteLine("			}")
+        LogW.WriteLine("			")
+        LogW.WriteLine("			th, td {")
+        LogW.WriteLine("				border: solid grey;")
+        LogW.WriteLine("				border-width: 1px 0 0 0;")
+        LogW.WriteLine("				padding: 1em;")
+        LogW.WriteLine("			}")
+        LogW.WriteLine("		</style>")
         LogW.WriteLine("	</head>")
         LogW.WriteLine("	<body>")
         LogW.WriteLine("		<h1>Create Synchronicity - Log for " & LogName & "</h1>")
@@ -67,37 +86,58 @@ Class LogHandler
     End Sub
 
     Sub PutLine(ByVal Title As String, ByVal Contents As String, ByRef LogW As IO.StreamWriter)
+#If DEBUG Then
+        LogW.WriteLine(Title & Microsoft.VisualBasic.vbTab & Contents)
+#Else
         LogW.WriteLine("<tr><td>" & Title & "</td><td>" & Contents & "</td></tr>")
+#End If
     End Sub
 
     Sub SaveAndDispose()
         If Disposed Then Exit Sub
         Disposed = True
 
-        Dim NewLog As Boolean = IO.File.Exists(ConfigOptions.GetLogPath(LogName))
+        Try
+            Dim NewLog As Boolean = Not IO.File.Exists(ConfigOptions.GetLogPath(LogName))
 
-        Dim LogWriter As New IO.StreamWriter(ConfigOptions.GetLogPath(LogName), True)
+            Dim LogWriter As New IO.StreamWriter(ConfigOptions.GetLogPath(LogName), True)
 
-        If NewLog Then OpenHTMLHeaders(LogWriter)
-        OpenSyncHeaders(LogWriter)
+            Try
+#If Not DEBUG Then
+                If NewLog Then OpenHTMLHeaders(LogWriter)
+                OpenSyncHeaders(LogWriter)
+#End If
 
 #If DEBUG Then
-        For Each Info As String In DebugInfo
-            PutLine("Info", Info, LogWriter)
-        Next
+                For Each Info As String In DebugInfo
+                    PutLine("Info", Info, LogWriter)
+                Next
 #End If
-        For Each Pair As KeyValuePair(Of SyncingItem, Boolean) In Log
-            PutLine(If(Pair.Value, "Succeded", "Failed"), Pair.Key.FormatType() & "	" & Pair.Key.FormatAction() & "	" & Pair.Key.Path, LogWriter)
-        Next
-        For Each Ex As Exception In Errors
-            PutLine("Error", Ex.Message & "	" & Ex.StackTrace.Replace(Microsoft.VisualBasic.vbNewLine, "\n"), LogWriter)
-        Next
+                For Each Pair As KeyValuePair(Of SyncingItem, Boolean) In Log
+                    PutLine(If(Pair.Value, "Succeded", "Failed"), Pair.Key.FormatType() & "	" & Pair.Key.FormatAction() & "	" & Pair.Key.Path, LogWriter)
+                Next
+                For Each Ex As Exception In Errors
+                    PutLine("Error", Ex.Message & "	" & Ex.StackTrace.Replace(Microsoft.VisualBasic.vbNewLine, "\n"), LogWriter)
+                Next
 
-        CloseSyncHeaders(LogWriter)
-        If NewLog Then CloseHTMLHeaders(LogWriter)
+#If Not DEBUG Then
+                CloseSyncHeaders(LogWriter)
+                If NewLog Then CloseHTMLHeaders(LogWriter)
+#End If
+            Catch Ex As Exception
+                Warning("Error writing to the log file!", Ex)
 
-        LogWriter.Flush()
-        LogWriter.Close()
-        LogWriter.Dispose()
+            Finally
+                LogWriter.Flush()
+                LogWriter.Close()
+                LogWriter.Dispose()
+            End Try
+        Catch Ex As Exception
+            Warning("Unable to open log file!", Ex)
+        End Try
+    End Sub
+
+    Sub Warning(ByVal Message As String, Optional ByVal Ex As Exception = Nothing)
+        MessageBox.Show(Message & Microsoft.VisualBasic.ControlChars.NewLine & If(Ex Is Nothing, "", Ex.Message))
     End Sub
 End Class
