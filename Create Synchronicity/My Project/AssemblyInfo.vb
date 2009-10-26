fc8f9b61a25ff87362d85c89166ca887e76f733b
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2009-10-26 08:24:43 +0000

    Forgot to update version numbers
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@128 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Create Synchronicity/My Project/AssemblyInfo.vb
+++ Create Synchronicity/My Project/AssemblyInfo.vb
@@ -41,11 +41,11 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("2.1.*")> 
+<Assembly: AssemblyVersion("2.2.*")> 
 #If DEBUG Then
-<Assembly: AssemblyFileVersion("2.09.10.17")> 
+<Assembly: AssemblyFileVersion("2.09.10.26")> 
 #Else
-<Assembly: AssemblyFileVersion("2.1.0.0")> 
+<Assembly: AssemblyFileVersion("2.2.0.0")> 
 #End If
 
 <Assembly: NeutralResourcesLanguageAttribute("")> 
