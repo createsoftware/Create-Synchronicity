284a6dc7c910f363db5db14b98140faff1de68bb
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-02-26 11:56:35 +0000

    Add scrollbars to the FlowLayoutPanel control if the contents do not fit.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@184 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Create Synchronicity/AboutForm.designer.vb
+++ Create Synchronicity/AboutForm.designer.vb
@@ -58,40 +58,52 @@ Partial Class AboutForm
         '
         'About_FlowLayoutPanel
         '
+        Me.About_FlowLayoutPanel.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.About_FlowLayoutPanel.AutoScroll = True
         Me.About_FlowLayoutPanel.Controls.Add(Me.About_LinkToProductPage)
         Me.About_FlowLayoutPanel.Controls.Add(Me.About_LinkToWebsite)
         Me.About_FlowLayoutPanel.Controls.Add(Me.About_VersionInfo)
         Me.About_FlowLayoutPanel.Controls.Add(Me.About_ContactLink)
         Me.About_FlowLayoutPanel.Controls.Add(Me.About_LinkToLicense)
         Me.About_FlowLayoutPanel.Controls.Add(Me.About_BugReport)
+        Me.About_FlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
         Me.About_FlowLayoutPanel.Location = New System.Drawing.Point(146, 12)
         Me.About_FlowLayoutPanel.Name = "About_FlowLayoutPanel"
-        Me.About_FlowLayoutPanel.Size = New System.Drawing.Size(328, 129)
+        Me.About_FlowLayoutPanel.Size = New System.Drawing.Size(344, 129)
         Me.About_FlowLayoutPanel.TabIndex = 1
+        Me.About_FlowLayoutPanel.WrapContents = False
         '
         'About_LinkToProductPage
         '
+        Me.About_LinkToProductPage.AutoSize = True
         Me.About_LinkToProductPage.Location = New System.Drawing.Point(3, 0)
+        Me.About_LinkToProductPage.MinimumSize = New System.Drawing.Size(0, 20)
         Me.About_LinkToProductPage.Name = "About_LinkToProductPage"
-        Me.About_LinkToProductPage.Size = New System.Drawing.Size(325, 20)
+        Me.About_LinkToProductPage.Size = New System.Drawing.Size(127, 20)
         Me.About_LinkToProductPage.TabIndex = 0
         Me.About_LinkToProductPage.TabStop = True
         Me.About_LinkToProductPage.Text = "Create Synchronicity"
         '
         'About_LinkToWebsite
         '
+        Me.About_LinkToWebsite.AutoSize = True
         Me.About_LinkToWebsite.Location = New System.Drawing.Point(3, 20)
+        Me.About_LinkToWebsite.MinimumSize = New System.Drawing.Size(0, 20)
         Me.About_LinkToWebsite.Name = "About_LinkToWebsite"
-        Me.About_LinkToWebsite.Size = New System.Drawing.Size(325, 20)
+        Me.About_LinkToWebsite.Size = New System.Drawing.Size(144, 20)
         Me.About_LinkToWebsite.TabIndex = 1
         Me.About_LinkToWebsite.TabStop = True
         Me.About_LinkToWebsite.Text = "\LINKTOWEBSITE_TEXT"
         '
         'About_VersionInfo
         '
+        Me.About_VersionInfo.AutoSize = True
         Me.About_VersionInfo.Location = New System.Drawing.Point(3, 40)
+        Me.About_VersionInfo.MinimumSize = New System.Drawing.Size(0, 20)
         Me.About_VersionInfo.Name = "About_VersionInfo"
-        Me.About_VersionInfo.Size = New System.Drawing.Size(325, 20)
+        Me.About_VersionInfo.Size = New System.Drawing.Size(98, 20)
         Me.About_VersionInfo.TabIndex = 2
         Me.About_VersionInfo.TabStop = True
         Me.About_VersionInfo.Text = "\VERSION_TEXT"
@@ -99,9 +111,11 @@ Partial Class AboutForm
         '
         'About_ContactLink
         '
+        Me.About_ContactLink.AutoSize = True
         Me.About_ContactLink.Location = New System.Drawing.Point(3, 60)
+        Me.About_ContactLink.MinimumSize = New System.Drawing.Size(0, 20)
         Me.About_ContactLink.Name = "About_ContactLink"
-        Me.About_ContactLink.Size = New System.Drawing.Size(325, 20)
+        Me.About_ContactLink.Size = New System.Drawing.Size(99, 20)
         Me.About_ContactLink.TabIndex = 3
         Me.About_ContactLink.TabStop = True
         Me.About_ContactLink.Text = "\CONTACT_LINK"
@@ -109,9 +123,11 @@ Partial Class AboutForm
         '
         'About_LinkToLicense
         '
+        Me.About_LinkToLicense.AutoSize = True
         Me.About_LinkToLicense.Location = New System.Drawing.Point(3, 80)
+        Me.About_LinkToLicense.MinimumSize = New System.Drawing.Size(0, 20)
         Me.About_LinkToLicense.Name = "About_LinkToLicense"
-        Me.About_LinkToLicense.Size = New System.Drawing.Size(325, 20)
+        Me.About_LinkToLicense.Size = New System.Drawing.Size(95, 20)
         Me.About_LinkToLicense.TabIndex = 4
         Me.About_LinkToLicense.TabStop = True
         Me.About_LinkToLicense.Text = "\LICENSE_TEXT"
@@ -119,10 +135,12 @@ Partial Class AboutForm
         '
         'About_BugReport
         '
+        Me.About_BugReport.AutoSize = True
         Me.About_BugReport.Location = New System.Drawing.Point(3, 100)
+        Me.About_BugReport.MinimumSize = New System.Drawing.Size(0, 20)
         Me.About_BugReport.Name = "About_BugReport"
         Me.About_BugReport.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.About_BugReport.Size = New System.Drawing.Size(325, 20)
+        Me.About_BugReport.Size = New System.Drawing.Size(87, 20)
         Me.About_BugReport.TabIndex = 5
         Me.About_BugReport.TabStop = True
         Me.About_BugReport.Text = "\BUG_REPORT"
@@ -130,15 +148,18 @@ Partial Class AboutForm
         '
         'About_Updates
         '
+        Me.About_Updates.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.About_Updates.Location = New System.Drawing.Point(12, 147)
         Me.About_Updates.Name = "About_Updates"
-        Me.About_Updates.Size = New System.Drawing.Size(462, 34)
+        Me.About_Updates.Size = New System.Drawing.Size(478, 34)
         Me.About_Updates.TabIndex = 2
         Me.About_Updates.Text = "\UPDATES"
         Me.About_Updates.UseVisualStyleBackColor = False
         '
         'About_LanguageLabel
         '
+        Me.About_LanguageLabel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
         Me.About_LanguageLabel.AutoSize = True
         Me.About_LanguageLabel.Location = New System.Drawing.Point(12, 190)
         Me.About_LanguageLabel.Name = "About_LanguageLabel"
@@ -149,17 +170,19 @@ Partial Class AboutForm
         '
         'About_LanguagesList
         '
+        Me.About_LanguagesList.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.About_LanguagesList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.About_LanguagesList.Location = New System.Drawing.Point(353, 187)
         Me.About_LanguagesList.Name = "About_LanguagesList"
-        Me.About_LanguagesList.Size = New System.Drawing.Size(121, 21)
+        Me.About_LanguagesList.Size = New System.Drawing.Size(137, 21)
         Me.About_LanguagesList.TabIndex = 4
         '
         'AboutForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(486, 220)
+        Me.ClientSize = New System.Drawing.Size(502, 220)
         Me.Controls.Add(Me.About_LanguageLabel)
         Me.Controls.Add(Me.About_LanguagesList)
         Me.Controls.Add(Me.About_Updates)
@@ -172,6 +195,7 @@ Partial Class AboutForm
         Me.Text = "\ABOUT"
         CType(Me.About_Logo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.About_FlowLayoutPanel.ResumeLayout(False)
+        Me.About_FlowLayoutPanel.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
