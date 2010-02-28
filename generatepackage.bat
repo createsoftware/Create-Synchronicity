dd28bc3a3436e920d1e3322693536d9db312d560
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-02-28 18:27:15 +0000

    Update headers to comply with GPL recommendations.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@198 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- generatepackage.bat
+++ generatepackage.bat
@@ -1,3 +1,11 @@
+REM This file is part of Create Synchronicity.
+REM 
+REM Create Synchronicity is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+REM Create Synchronicity is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+REM You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see <http://www.gnu.org/licenses/>.
+REM Created by:	Clément Pit--Claudel.
+REM Web site:		http://synchronicity.sourceforge.net.
+
 "C:\Program Files\NSIS\makensis.exe" "Create Synchronicity\setup_script.nsi"
 cd "Create Synchronicity\bin\Release"
 "C:\Program Files\7-Zip\7z.exe" a "..\..\..\Create_Synchronicity.zip" "*"
