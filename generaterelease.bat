a5c8e960a8b8a84aa5f0b72280c0dbf946996a6c
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-04-20 08:42:00 +0000

    Add release script
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@344 6fc22aba-598d-424d-8c45-4c47160ecdfc

---
+++ generaterelease.bat
@@ -0,0 +1,28 @@
+REM This file is part of Create Synchronicity.
+REM 
+REM Create Synchronicity is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+REM Create Synchronicity is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+REM You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see <http://www.gnu.org/licenses/>.
+REM Created by:	Clément Pit--Claudel.
+REM Web site:		http://synchronicity.sourceforge.net.
+
+@set VER=%1
+mkdir build
+
+(
+echo Packaging log for r%REV% & date /t & time /t
+
+echo.
+echo -----
+"C:\Program Files\NSIS\makensis.exe" "Create Synchronicity\setup_script.nsi"
+
+echo.
+echo -----
+move Create_Synchronicity_Setup.exe "Create_Synchronicity_Setup-%VER%_Setup.exe"
+
+cd "Create Synchronicity\bin\Release"
+echo.
+echo -----
+"C:\Program Files\7-Zip\7z.exe" a "..\..\..\Create_Synchronicity-%VER%.zip" "Create Synchronicity.exe" "COPYING" "Release notes.txt" "languages\*"
+cd ..\..\..
+) > "build\buildlog-v%VER%.txt"
