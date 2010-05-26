1ffc167042fe30c66445e3f34f21cb82d7a15098
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-05-26 19:59:49 +0000

    Update packaging script with a help menu.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@483 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- generatepackage.bat
+++ generatepackage.bat
@@ -1,10 +1,13 @@
-REM This file is part of Create Synchronicity.
-REM 
-REM Create Synchronicity is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-REM Create Synchronicity is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-REM You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see <http://www.gnu.org/licenses/>.
-REM Created by:	Clément Pit--Claudel.
-REM Web site:		http://synchronicity.sourceforge.net.
+@if "%1" == "/?" goto help
+
+:start
+@echo This file is part of Create Synchronicity.
+@echo 
+@echo Create Synchronicity is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+@echo Create Synchronicity is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+@echo You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see <http://www.gnu.org/licenses/>.
+@echo Created by:	Clément Pit--Claudel.
+@echo Web site:     http://synchronicity.sourceforge.net.
 
 @set REV=%4
 mkdir build
@@ -26,3 +29,10 @@ echo -----
 "C:\Program Files\7-Zip\7z.exe" a "..\..\..\build\Create_Synchronicity-r%REV%.zip" "Release notes.txt" "Create Synchronicity.exe" "COPYING" "languages\*"
 cd ..\..\..
 ) > "build\buildlog-r%REV%.txt"
+@goto end
+
+:help
+@echo This script is designed to be called as a SVN hook script.
+@echo If used from the command line, it should be passed the revision number as its fourth parameter.
+
+:end
