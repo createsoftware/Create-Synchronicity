af50cb861aca47e485516ac3b7b374254e73a9b6
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-06-05 22:35:58 +0000

    Update vernum
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@508 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Create Synchronicity/My Project/AssemblyInfo.vb
+++ Create Synchronicity/My Project/AssemblyInfo.vb
@@ -43,7 +43,7 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyVersion("3.5.*")> 
 #If DEBUG Then
-<Assembly: AssemblyFileVersion("3.2010.6.5")> 
+<Assembly: AssemblyFileVersion("3.2010.6.6")> 
 #Else
 <Assembly: AssemblyFileVersion("3.5.0.0")> 
 #End If
