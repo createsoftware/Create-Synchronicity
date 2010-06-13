1c253dcc199b64de01f26f76f8489a127e66c6c2
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-06-13 13:45:26 +0000

    Fix unregistering program at boot.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@546 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Create Synchronicity/MainForm.vb
+++ Create Synchronicity/MainForm.vb
@@ -377,7 +377,7 @@ Public Class MainForm
 
         Try
             If Not NeedToRunAtBootTime Then
-                If My.Computer.Registry.GetValue(ConfigOptions.RegistryRootedBootKey, ConfigOptions.RegistryBootVal, Nothing) IsNot Nothing Then My.Computer.Registry.CurrentUser.OpenSubKey(ConfigOptions.RegistryBootKey, True) 'TODO: ???
+                If My.Computer.Registry.GetValue(ConfigOptions.RegistryRootedBootKey, ConfigOptions.RegistryBootVal, Nothing) IsNot Nothing Then My.Computer.Registry.CurrentUser.OpenSubKey(ConfigOptions.RegistryBootKey, True).DeleteValue(ConfigOptions.RegistryBootVal) 'TODO: ???
             End If
         Catch Ex As Exception
             Interaction.ShowMsg(Translation.Translate("\UNREG_ERROR"), Translation.Translate("\ERROR"), MessageBoxButtons.OK, MessageBoxIcon.Error)
