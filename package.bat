66167eefe27cd63aed3efd20308683464cd76f66
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-07-23 13:22:07 +0000

    Remove line feed, fix url, use whitespaces.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@656 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- package.bat
+++ package.bat
@@ -2,11 +2,10 @@
 
 :start
 @echo This file is part of Create Synchronicity.
-@echo 
 @echo Create Synchronicity is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 @echo Create Synchronicity is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 @echo You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see http://www.gnu.org/licenses/.
-@echo Created by:	ClÃ©ment Pit--Claudel.
+@echo Created by:   Clément Pit--Claudel.
 @echo Web site:     http://synchronicity.sourceforge.net.
 
 @set REV=%1
