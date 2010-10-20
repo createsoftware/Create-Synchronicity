1684f9594b42acce32a6dfdd0c4a2523aff87ebf
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-10-20 17:31:15 +0000

    remove extra carets Better manual build script.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@811 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- manual.bat
+++ manual.bat
@@ -4,11 +4,17 @@
 @echo This file is part of Create Synchronicity.
 @echo Create Synchronicity is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 @echo Create Synchronicity is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-@echo You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see <http://www.gnu.org/licenses/>.
+@echo You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see http://www.gnu.org/licenses/.
 @echo Created by:   Clément Pit--Claudel.
 @echo Web site:     http://synchronicity.sourceforge.net.
 
-Xhtml2Latex.exe "..\..\..\..\..\Sites Web\Sourceforge\Synchronicity\pages\help.php" "build\Create Synchronicity User Manual.tex" "http://synchronicity.sourceforge.net/" "..\..\..\..\..\Sites Web\Sourceforge\Synchronicity\\" /addroot
+@set WEBROOT=..\..\..\..\..\Sites Web\Sourceforge\Synchronicity
+@set WEBFOLDER=%WEBROOT%\pages
+
+Xhtml2Latex.exe "%WEBFOLDER%\help.php" "%WEBFOLDER%\help.tex" "http://synchronicity.sourceforge.net/" "%WEBROOT%\\" /addroot
+copy "%WEBFOLDER%\help.tex" "build\Create Synchronicity User Manual.tex"
+copy "%WEBFOLDER%\help.pdf" "build\Create Synchronicity User Manual.pdf"
+
 @goto end
 
 :help
--- release.bat
+++ release.bat
@@ -4,7 +4,7 @@
 @echo This file is part of Create Synchronicity.
 @echo Create Synchronicity is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 @echo Create Synchronicity is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-@echo You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see <http://www.gnu.org/licenses/>.
+@echo You should have received a copy of the GNU General Public License along with Create Synchronicity.  If not, see http://www.gnu.org/licenses/.
 @echo Created by:   Clément Pit--Claudel.
 @echo Web site:     http://synchronicity.sourceforge.net.
 
