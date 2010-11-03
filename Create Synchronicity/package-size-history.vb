39499e4f3254bbf643adbf7d465a63f9c1402eac
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-11-03 13:04:07 +0000

    Update package statistics script
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@913 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Create Synchronicity/package-size-history.vb
+++ Create Synchronicity/package-size-history.vb
@@ -1,16 +1,18 @@
 Imports System
+Imports System.Collections.Generic
+Imports System.Text.RegularExpressions
 
 Module PackageSize
     Structure PackageInfo
         Dim Id As Integer
         Dim Name As String
         Dim Size As Long
-        Shared GlobalRegex As String = "Create_Synchronicity(_Setup)?-r(\d+)(-DEBUG)?\.(zip|exe)"
+        Shared GlobalRegex As String = "Create_Synchronicity(_Setup)?-r(\d+)(-DEBUG)?"
 
         Sub New(ByVal Path As String)
             Name = IO.Path.GetFileNameWithoutExtension(Path)
             Size = (New System.IO.FileInfo(Path)).Length
-            Id = CInt(Text.RegularExpressions.Regex.Replace(Name, GlobalRegex, "$2"))
+            Id = CInt(Regex.Replace(Name, GlobalRegex, "$2"))
         End Sub
     End Structure
 
@@ -19,26 +21,31 @@ Module PackageSize
         Dim DebugZipFiles As New List(Of PackageInfo)
         Dim SetupFiles As New List(Of PackageInfo)
 
-
-        For Each File As String In IO.Directory.GetFiles("..\build\", "", IO.SearchOption.AllDirectories)
+        ' VB.Net executable directory: System.AppDomain.CurrentDomain.BaseDirectory
+        For Each File As String In IO.Directory.GetFiles(System.AppDomain.CurrentDomain.BaseDirectory, "*.*", IO.SearchOption.AllDirectories)
             Dim FName As String = IO.Path.GetFileName(File)
 
-            If Text.RegularExpressions.Regex.IsMatch(FName, "Create_Synchronicity-r\d+\.zip") Then
+            If Regex.IsMatch(FName, "Create_Synchronicity-r\d+\.zip") Then
                 ZipFiles.Add(New PackageInfo(File))
-            ElseIf Text.RegularExpressions.Regex.IsMatch(FName, "Create_Synchronicity-r\d+-DEBUG\.zip") Then
+            ElseIf Regex.IsMatch(FName, "Create_Synchronicity-r\d+-DEBUG\.zip") Then
                 DebugZipFiles.Add(New PackageInfo(File))
-            ElseIf Text.RegularExpressions.Regex.IsMatch(FName, "Create_Synchronicity_SETUP-r\d+\.exe") Then
+            ElseIf Regex.IsMatch(FName, "Create_Synchronicity_Setup-r\d+\.exe") Then
                 SetupFiles.Add(New PackageInfo(File))
             End If
         Next
 
-        Dim Print = Sub(Package As PackageInfo) Console.WriteLine("{0}	{1}", Package.Id, Package.Size)
+        Dim PrintRoutine = Sub(Package As PackageInfo) Console.WriteLine("{0}	{1}", Package.Id, Package.Size)
+        Dim Comparer As New System.Comparison(Of PackageInfo)(Function(PI1 As PackageInfo, PI2 As PackageInfo) PI1.Id < PI2.Id)
 
-        Console.WriteLine("== Zip Files ==")
-        ZipFiles.ForEach(Print)
-        Console.WriteLine("== DEBUG Zip Files ==")
-        ZipFiles.ForEach(Print)
-        Console.WriteLine("== Setup Files ==")
-        ZipFiles.ForEach(Print)
+        ZipFiles.Sort(Comparer)
+        DebugZipFiles.Sort(Comparer)
+        SetupFiles.Sort(Comparer)
+
+        Console.WriteLine("Rev	Size    (Zip)")
+        ZipFiles.ForEach(PrintRoutine)
+        Console.WriteLine("Rev	Size    (DEBUG Zip)")
+        DebugZipFiles.ForEach(PrintRoutine)
+        Console.WriteLine("Rev	Size    Setup")
+        SetupFiles.ForEach(PrintRoutine)
     End Sub
 End Module
