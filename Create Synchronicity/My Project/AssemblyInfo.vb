5dad433d22afb7ad6d39fa0f81efc1c167dfd37f
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-11-07 07:07:16 +0000

    VS 2010 definitely has a problem with file stability.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@928 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Create Synchronicity/My Project/AssemblyInfo.vb
+++ Create Synchronicity/My Project/AssemblyInfo.vb
@@ -43,7 +43,7 @@ Imports System.Runtime.InteropServices
 
 <Assembly: AssemblyVersion("5.0.*")> 
 #If DEBUG Then
-<Assembly: AssemblyFileVersion("5.2010.11.6")> 
+<Assembly: AssemblyFileVersion("5.2010.11.7")> 
 #Else
 <Assembly: AssemblyFileVersion("5.0.0.0")> 
 #End If
