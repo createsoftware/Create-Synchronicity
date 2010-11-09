dd7b1d9d0562e26b750d10aecf3704ef464bdadd
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-11-09 19:14:53 +0000

    Update svn_post_commit.bat
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@929 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- svn_post_commit.bat
+++ svn_post_commit.bat
@@ -5,3 +5,4 @@ set ver=%ver:"###=%
 set ver=%ver:###"=%
 set ver=%ver:###=%
 package.bat %ver% > buildlog.txt
+post-commit.exe http://synchronicity.sourceforge.net/code/post-commit.php createsoftware synchronicity %*
