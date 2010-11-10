fe671aeb84995d4a53e3d42844537f097a0e51c6
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-11-10 18:09:15 +0000

    Fix post-commit script.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@932 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- svn_post_commit.bat
+++ svn_post_commit.bat
@@ -5,4 +5,4 @@ set ver=%ver:"###=%
 set ver=%ver:###"=%
 set ver=%ver:###=%
 package.bat %ver% > buildlog.txt
-post-commit.exe http://synchronicity.sourceforge.net/code/post-commit.php createsoftware synchronicity %* > sql-log.txt
+spawn post-commit.exe http://synchronicity.sourceforge.net/code/post-commit.php sql-log.txt createsoftware synchronicity %*
