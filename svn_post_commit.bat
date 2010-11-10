591e3e22723a51c3486da74f3074e46a34762eb2
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-11-10 23:41:52 +0000

    Fix parameters order
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@933 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- svn_post_commit.bat
+++ svn_post_commit.bat
@@ -5,4 +5,4 @@ set ver=%ver:"###=%
 set ver=%ver:###"=%
 set ver=%ver:###=%
 package.bat %ver% > buildlog.txt
-spawn post-commit.exe http://synchronicity.sourceforge.net/code/post-commit.php sql-log.txt createsoftware synchronicity %*
+spawn-visual post-commit.exe sql-log.txt http://synchronicity.sourceforge.net/code/post-commit.php createsoftware synchronicity %*
