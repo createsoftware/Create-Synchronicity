0a09abac8c1c34ed671d0496f9b87b25c945559d
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2010-12-17 06:56:48 +0000

    Update svn_post_commit.bat
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@1012 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- svn_post_commit.bat
+++ svn_post_commit.bat
@@ -1,8 +1,2 @@
 cd /D "D:\Documents\Programmes\Visual Basic\Visual Studio\Create Synchronicity\trunk\"
-set ver=%4
-set ver=###%ver%###
-set ver=%ver:"###=%
-set ver=%ver:###"=%
-set ver=%ver:###=%
-package.bat %ver% > buildlog.txt
 post-commit.exe sql-log.txt http://synchronicity.sourceforge.net/code/post-commit.php createsoftware synchronicity %*
