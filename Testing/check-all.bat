8eff3744c9a0a9d8c43651d4e95837e5ff187035
createsoftware <createsoftware@6fc22aba-598d-424d-8c45-4c47160ecdfc>
2011-05-01 08:47:55 +0000

    Output more details when checking.
    git-svn-id: https://synchronicity.svn.sourceforge.net/svnroot/synchronicity/trunk@1306 6fc22aba-598d-424d-8c45-4c47160ecdfc

--- Testing/check-all.bat
+++ Testing/check-all.bat
@@ -1,7 +1,11 @@
+@echo off
 REM Test profiles use relative paths, and thus must be called from the right folder.
+
+echo ** Synchronizing **
 cd bin
 "Create Synchronicity.exe" /quiet /run "Fedora 15|Linux|Subversion|Vlc"
 cd ..
+echo ** Checking results **
 call check Fedora-15-Beta-x86_64-DVD
 call check subversion
 call check linux-2.6
--- Testing/check.bat
+++ Testing/check.bat
@@ -1,1 +1,4 @@
-call .\fdiff\fdiff.bat %1 destination\%1 %1
+@echo * Checking %1
+@call .\fdiff\fdiff.bat %1 destination\%1 %1
+@echo * Done.
+@echo.
